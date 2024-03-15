8ad180e1427208512dca0d628c409dc3d65f5633
Anatolii <you@example.com>
2024-03-15 15:38:35 +0200

    Increase token ttl

--- src/auth/dto/credentials.dto.ts
+++ src/auth/dto/credentials.dto.ts
@@ -5,6 +5,11 @@ import { ApiProperty } from '@nestjs/swagger';
 const USERNAME_MAX_LENGTH = 50;
 const USERNAME_MIN_LENGTH = 6;
 const PASSWORD_MAX_LENGTH = 50;
+const PASSWORD_MIN_LENGTH = 8;
+
+const passwordRegex = new RegExp(
+    `(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{${PASSWORD_MIN_LENGTH},${PASSWORD_MAX_LENGTH}}`
+);
 
 export class CredentialsDto implements Credentials {
   @IsString()
@@ -17,10 +22,11 @@ export class CredentialsDto implements Credentials {
   username: string;
 
   @IsString()
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
-    message: 'password is too weak',
+  @Matches(passwordRegex, {
+    message: 'Password must include at least one lowercase letter, one uppercase letter, one number, and one special character.',
   })
   @MaxLength(PASSWORD_MAX_LENGTH)
+  @MinLength(PASSWORD_MIN_LENGTH)
   @ApiProperty()
   password: string;
 }
